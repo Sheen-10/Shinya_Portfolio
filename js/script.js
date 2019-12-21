f287dd7cfac708ea7e74bdd8b127b36ffc1d06cc
Shinya Mori <shinya199905102525@gmail.com>
2019-12-21 17:26:47 +0800

    Fixed aboutme page

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(function(){
+    var pagetop = $('#page_top');
+    // ボタン非表示
+    pagetop.hide();
+
+    $(window).scroll(function () {
+    if ($(this).scrollTop() > 80) {
+            pagetop.fadeIn();
+    } else {
+            pagetop.fadeOut();
+    }
+    });
+    pagetop.click(function () {
+    $('body, html').animate({ scrollTop: 0 }, 500);
+    return false;
+    });
+});
